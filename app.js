e526586df4855d14765cd2b33b7483b5f954f915
lxe <lxe@lxe.co>
2012-09-29 16:49:47 -0400

    Added some comments

--- app.js
+++ app.js
@@ -19,10 +19,18 @@ var _          = require('underscore')
 var app = module.exports = express()
 
 app.configure(function() {
+
   /**
-   * 
+   * Use the connect-less parser. 
    */
   app.set('views', __dirname + '/views')
+
+  /**
+   * Use the jade compiler. This one
+   * converts all the jade files to css before serving
+   * and places them into /public/stylehseets
+   * directory. I'm sure it's got some sort of caching mechanism as well.
+   */
   app.set('view engine', 'jade')
   app.use(require('connect-less')({
     src: __dirname + '/views',
@@ -31,16 +39,20 @@ app.configure(function() {
   }))
 
   /**
-   * 
+   * Set up static paths
    */
   app.use(express.static(__dirname + '/public'))
   app.use(express.favicon(__dirname + '/public/images/favicon.ico'))
+
+  /**
+   * Pre-packaged miscellaneous middlewares
+   */
   app.use(express.bodyParser())
   app.use(express.methodOverride())
   app.use(express.cookieParser())
 
   /**
-   * 
+   * Store sessions in mongo
    */
   app.use(express.session({
     secret: 'whalecopter',
@@ -48,17 +60,18 @@ app.configure(function() {
     store:  new MongoStore({ db: 'clickbin' })
   }))
 
-
   /**
-   * [ description]
-   * @param  {[type]}   req  [description]
-   * @param  {[type]}   res  [description]
-   * @param  {Function} next [description]
-   * @return {[type]}        [description]
+   * Custom middleware
    */
   app.use(function(req, res, next) {
 
-    // Flash 
+    /**
+     * Custom "flash" middleware. Unlike the
+     * original connect-flash 
+     * (https://github.com/jaredhanson/connect-flash),
+     * this one is simpler, and allows any sort
+     * of objects, not just flat strings.
+     */
     req.session.flash = req.session.flash || {}
     res.locals.flash  = req.session.flash
     req.session.flash = {}
@@ -66,15 +79,11 @@ app.configure(function() {
   })
 
   /**
-   * 
+   * Router middleware
    */
   app.use(app.router)
 })
 
-/**
- * [ description]
- * @return {[type]} [description]
- */
 app.configure('development', function() {
   app.use(express.errorHandler({
     dumpExceptions: true,
@@ -82,10 +91,6 @@ app.configure('development', function() {
   }))
 })
 
-/**
- * [ description]
- * @return {[type]} [description]
- */
 app.configure('production', function() {
   app.use(express.errorHandler())
 })
@@ -95,8 +100,7 @@ require('./routes')(app)
 
 /**
  * ============================================================================
- * [serverConfig description]
- * @type {Object}
+ * Go go go go!
  */
 mongo.connect('mongodb://localhost/clickbin')
 mongo.connection.on('open', function() {
