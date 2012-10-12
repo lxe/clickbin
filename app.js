0a2df245ff4b0c640170acc21912d827baa433db
victor <victor@yinzcam.com>
2012-10-12 00:10:36 -0400

    added logging

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ var _          = require('underscore')
 var app = module.exports = express()
 
 app.configure(function() {
-
+  
+  if(config.logging){
+    app.use(function(req, res, next){
+      console.log('%s %s %s', new Date(), req.method, req.url)
+      next()
+    })
+  }
+  
   /**
    * Use the connect-less parser. 
    */
--- config.js
+++ config.js
@@ -7,14 +7,28 @@ var config = {
   , maxRequestSize : 2097152 /*2mb*/ // the max size of a piece of requested resource
   , userAgent : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) '
     + 'AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+  , logging : true
 }
+
+// "In an extreme view, the world can be seen as only connections, nothing 
+// else. We think of a dictionary as the repository of meaning, but it defines 
+// words only in terms of other words. I liked the idea that a piece of 
+// information is really defined only by what it's related to, and how it's 
+// related. There really is little else to meaning. The structure is 
+// everything. There are billions of neurons in our brains, but what are 
+// neurons? Just cells. The brain has no knowledge until connections are made 
+// between neurons. All that we know, all that we are, comes from the way our 
+// neurons are connected."
+//                             - Tim Berners-Lee, inventor of the world wide web
 
 var argv = require('optimist')
   .default('port',config.port)
   .default('domain',config.domain)
+  .default('logging',config.logging)
   .argv
 
 config.server.port = argv.port
 config.domain = argv.domain
+config.logging = argv.logging
 
 module.exports = config
