509d7c0cf2403938838931bce99a0288ee42c39f
victor <victor@yinzcam.com>
2012-10-17 00:27:09 -0400

    prevent the nav from disapearing on mobile. added host-specific scrapers for wikipedia and flickr

---
+++ controllers/hostname-specific-scrapers/wikipedia.org.js
@@ -0,0 +1,26 @@
+
+var _ = require('underscore')
+
+/**
+  * twitter specific scraper
+  */
+
+module.exports = function(url,$){
+  var page = {}
+  // try to get the best title from the page
+  _.any(['title','h1','h2','h3','h4','h5','h6'],function(tag){
+    tag = $(tag)
+    if(tag.length) page.title = tag.first().text()
+    return page.title // stop if we found a title
+  })
+  
+  page.icon = $('#mw-content-text a.image img')
+  if(page.icon.length && page.icon.attr('width') >= 200){
+    page.icon = page.icon.first().attr('src') // could still be empty...
+    // wikipedia's got some weird image links... missing protocol
+    if(page.icon && page.icon.length > 2 && page.icon.substr(0,2) === '//' )
+      page.icon = 'http:' + page.icon
+  }else page.icon = null
+  
+  return page
+}
