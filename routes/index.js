ff4bb8166dc427aee027b55152f5c5ecc3f69ae0
lxe@github.com <lxe@lxe.co>
2012-10-22 18:49:17 -0400

    Added better UI

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,45 @@
+var fs = require('fs'),
+  user = require('./user')
 
-var fs = require('fs')
-  , user = require('./user')
+  module.exports = function(app) {
 
-module.exports = function(app) {
-  
-  app.get('/', function(req, res, next) {
-    var host = req.get('Host').split('.').reverse()
-    // if the request contains a subdomain use the `user` route
-    if(req.subdomains.length!==0){
-      return user(req,res,next,{
-        username : req.subdomains[0]
-        , path : '/'
-      })
-    }
-    // if the user is not logged in
-    if(!req.session.user || !req.session.user.loggedIn){
-      // show the landing page
-      return res.render('landing', { title : 'ClickBin' })
-    }else{
-      console.log('redirecting to the logged in users page')
-      // redirect to the users bin
-      var url =  req.protocol + '://' + req.session.user.username 
-        + '.' + host[1] + '.' + host[0] + '/'
-      return res.redirect(url)
-    }
-  })
-  
-  require('./login')(app)
-  require('./logout')(app)
-  require('./signup')(app)
-  require('./link')(app)
-  require('./bin')(app)
-  require('./path')(app)
-}
+    app.get('/', function(req, res, next) {
+      var host = req.get('Host').split('.').reverse()
+      console.log(host)
+
+      // if the request contains a subdomain use the `user` route
+      if(req.subdomains.length !== 0) {
+        return user(req, res, next, {
+          username: req.subdomains[0],
+          path: '/'
+        })
+      }
+
+      // if the user is not logged in
+      if(!req.session.user || !req.session.user.loggedIn) {
+
+        // show the landing page
+        return res.render('landing', {
+          title: 'ClickBin',
+          bodyId: 'landing'
+        })
+      } else {
+        console.log('redirecting to the logged in users page')
+
+        // redirect to the users bin
+        var url = req.protocol + '://' 
+          + req.session.user.username 
+          + '.' + host[1] + '.' + host[0] + '/';
+
+        console.log(url)
+        return res.redirect(url)
+      }
+    })
+
+    require('./login')(app)
+    require('./logout')(app)
+    require('./signup')(app)
+    require('./link')(app)
+    require('./bin')(app)
+    require('./path')(app)
+  }
