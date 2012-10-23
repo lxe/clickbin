5971ff6472af8e79246c3524cac7018059b6105c
lxe@github.com <lxe@lxe.co>
2012-10-22 22:58:03 -0400

    Added new UI

--- public/_/javascripts/client.js
+++ public/_/javascripts/client.js
@@ -1,6 +1,22 @@
-$(function(){
+$(function() {
   $(".ellipsis").ellipsis();
-  window.onresize = function(){
+
+  window.onresize = function() {
     $(".ellipsis").ellipsis();
+  }
+
+  var newItemForm = $('#new-item-form');
+  if (newItemForm) {
+    var newItemTitle = $('#new-item-title');
+
+    newItemForm.submit(function(event) {
+      event.preventDefault();
+      var item = newItemTitle.val();
+      if (item !== '') {
+        window.location.href += '/' + newItemTitle.val();
+      }
+      return false;
+    });
+
   }
 })
