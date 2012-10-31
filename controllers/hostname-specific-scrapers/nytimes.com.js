296cc2017b8e6b79cf048c8644c0416200af63d8
victor <victor@yinzcam.com>
2012-10-31 12:09:33 -0400

    improved support for nytimes scraper

--- controllers/hostname-specific-scrapers/nytimes.com.js
+++ controllers/hostname-specific-scrapers/nytimes.com.js
@@ -15,6 +15,7 @@ module.exports = function(url,parts,$){
   })
   
   page.icon = $('.articleSpanImage img')
+  if(!page.icon.length) page.icon = $('.inlineImage img')
   if(page.icon.length){
     page.icon = page.icon.first().attr('src')
   }else page.icon = null
