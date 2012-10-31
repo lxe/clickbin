c9c0618f9fc2cf9e60aa0fe46324f8eb9ee67a78
victor <victor@yinzcam.com>
2012-10-31 17:32:01 -0400

    added mascot

--- routes/link.js
+++ routes/link.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get('/_/link/add.jsonp', function(req, res, next) {
     console.log('link/add.jsonp')
     if(req.accepts('text/javascript')){
-      // TODO... req.query
+      // TODO: examin the `req.query` and perform the approviate action
       return res.jsonp({
         path : req.session.bookmarkletPath
       })
