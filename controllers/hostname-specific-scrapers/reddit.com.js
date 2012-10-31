a6d7c1e0911fae3b246c70e1565f179fa7501ce5
victor <victor@yinzcam.com>
2012-10-31 14:41:00 -0400

    added reddit specific scraper

---
+++ controllers/hostname-specific-scrapers/reddit.com.js
@@ -0,0 +1,23 @@
+
+var _ = require('underscore')
+
+/**
+  * instagram specific scraper
+  */
+
+module.exports = function(url,parts,$){
+  var page = {}
+  // try to get the best title from the page
+  _.any(['title','h1','h2','h3','h4','h5','h6'],function(tag){
+    tag = $(tag)
+    if(tag.length) page.title = tag.first().text()
+    return page.title // stop if we found a title
+  })
+  
+  page.icon = $('.thumbnail img')
+  if(page.icon.length){
+    page.icon = page.icon.first().attr('src') // could still be empty...
+  }else page.icon = null
+  
+  return page
+}
